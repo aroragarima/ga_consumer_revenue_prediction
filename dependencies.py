7cc15dc13454170a0183c2ee3088886c22ea2a84
aroragarima <garora2k9@gmail.com>
2019-03-18 13:03:28 +0000

    added dependecies.py and data partitioning script

---
+++ dependencies.py
@@ -0,0 +1,22 @@
+# required for increasing the field size
+import os 
+import csv
+# csv.field_size_limit(sys.maxsize)
+
+import json
+import numpy as np
+import pandas as pd
+from pandas.io.json import json_normalize
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+
+from plotly import tools
+import plotly.offline as py
+import plotly.graph_objs as go
+
+from sklearn import model_selection, preprocessing, metrics
+import lightgbm as lgb
+
+pd.options.mode.chained_assignment = None
+pd.options.display.max_columns = 999
---
+++ read_data.py
@@ -0,0 +1,37 @@
+# from dependencies import *
+
+import pandas as pd
+import json
+from pandas.io.json import json_normalize
+
+# required for increasing the field size
+import os
+import csv
+import random
+# csv.field_size_limit(sys.maxsize)
+
+columns = ['device', 'geoNetwork', 'totals', 'trafficSource']  # json columns
+
+dir_path = "datafiles/"
+p = 0.4
+
+def json_read(df):
+    data_frame = dir_path + df
+
+    df = pd.read_csv(data_frame,
+                     converters={column: json.loads for column in columns},
+                     dtype={'fullVisitorId': 'str'},
+                     skiprows=lambda i: i>0 and random.random() > p)
+
+    for column in columns:
+        column_as_df = json_normalize(df[column])
+        column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
+        df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
+
+    print(f"Loaded {os.path.basename(data_frame)}. Shape: {df.shape}")
+    return df
+
+
+sampled_data = json_read("train_v2.csv")
+
+sampled_data.to_csv(r"train_sampled_v40perc.csv")
