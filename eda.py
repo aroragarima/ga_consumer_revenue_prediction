fba1a882056312b89fb9d05a7bd4e923524c1019
aroragarima <garora2k9@gmail.com>
2019-03-23 13:00:17 +0000

    target variable exploration wip

--- eda.py
+++ eda.py
@@ -1,19 +1,18 @@
-from dependencies.py import *
+from dependencies import *
 
 train_df = pd.read_csv("datafiles/train_sampled_v40perc.csv")
 
 def target_variable_exploration(df):
+	df["totals.transactionRevenue"] = df["totals.transactionRevenue"].astype('float')
+	gdf = df.groupby("fullVisitorId")["totals.transactionRevenue"].sum().reset_index()
 
-	train_df["totals.transactionRevenue"] = train_df["totals.transactionRevenue"].astype('float')
-	gdf = train_df.groupby("fullVisitorId")["totals.transactionRevenue"].sum().reset_index()
-
-#	TODO: define a helper function for plotting graphs
+	#TODO: define a helper function for plotting graphs
 
 	plt.figure(figsize=(8,6))
-	plt.scatter(range(gdf.shape[0]), np.sort(np.log1p(gdf["totals.transactionRevenue"].values)))
+	plt.scatter(range(gdf.shape[0]), np.sort(gdf["totals.transactionRevenue"].values))
 	plt.xlabel('index', fontsize=12)
 	plt.ylabel('TransactionRevenue', fontsize=12)
 	plt.show()
-	plt.savefig(("target_variable_exploration.png")
+	plt.savefig(("target_variable_exploration_sans_log.png"))
 
 target_variable_exploration(train_df)
