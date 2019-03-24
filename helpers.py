983578e3075abe371b91bb63ac6fcf80558de91f
aroragarima <garora2k9@gmail.com>
2019-03-24 19:25:12 +0000

    Added Browser, Device, OS plots

--- helpers.py
+++ helpers.py
@@ -4,5 +4,4 @@ def plot_horizontally(x, y, filename):
 	plt.figure(figsize=(4,5))
 	plt.title(filename)
 	sns.barplot(x=x, y=y, orient = "h")
-	plt.savefig(filename, dpi=200)
-	print(filename)
+	plt.savefig("graph_plots/"+filename, dpi=100)
